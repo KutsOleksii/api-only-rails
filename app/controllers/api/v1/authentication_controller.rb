723d2d7af6f1ae21475371e1961f727c173a0d2b
Lexus <oleksii.kuts@faceit-team.com>
2023-11-13 09:59:00 +0200

    tutorial is complete

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,19 +1,30 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
+      class AuthenticationError < StandardError; end
+
       rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      rescue_from AuthenticationError, with: :handle_unauthenticated
 
       def create
-        p params.require(:password).inspect
+        raise AuthenticationError unless user.authenticate(params.require(:password))
+        token = AuthenticationTokenService.encode(user.id)
 
-        user = User.find_by(username: params.require(:username))
-        AuthenticationTokenService.call(user.id)
+        render json: {token: token }, status: :created
+      end
 
-        render json: {token: '123' }, status: :created
+      private
+
+      def user
+        @user ||= User.find_by(username: params.require(:username))
       end
 
       def parameter_missing(e)
         render json: { error: e.message}, status: :unprocessable_entity
+      end
+
+      def handle_unauthenticated
+        head :unauthorized
       end
     end
   end
